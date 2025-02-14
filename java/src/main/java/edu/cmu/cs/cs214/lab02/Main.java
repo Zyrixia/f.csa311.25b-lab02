b238fa0994dc31674d6990181cff15c496015f20
Zyrixia <zyrixiaemail@gmail.com>
2025-02-14 10:22:02 +0800

    improved data encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(8);
+        Shape circle = new Circle(5);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer rectRenderer = new Renderer(rectangle);
+        Renderer circRenderer = new Renderer(circle);
+        Renderer sqrRenderer = new Renderer(square);
 
-        renderer.draw();
+        rectRenderer.draw();
+        circRenderer.draw();
+        sqrRenderer.draw();
     }
 }
